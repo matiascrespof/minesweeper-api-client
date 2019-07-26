ecccbb26596706efb9ab96a8306032a55f4b103f
Name <you@example.com>
2019-07-26 18:39:48 -0300

    update to secure protocol

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://minesweeper-mcrespo.herokuapp.com/minesweeper/",
+  baseURL: "https://minesweeper-mcrespo.herokuapp.com/minesweeper/",
   //baseURL: "http://localhost:8080/minesweeper",
   headers: {
     "Content-type": "application/json",
